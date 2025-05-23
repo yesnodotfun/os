6fdae1621a23bcdca6ed3532b6a8abd77e29f260
Ryo Lu <me@ryo.lu>
2025-05-23 14:26:19 -0700

    Remove WINDOW_SWAP sound from useSound hook

--- src/hooks/useSound.ts
+++ src/hooks/useSound.ts
@@ -170,7 +170,6 @@ export const Sounds = {
   WINDOW_OPEN: "/sounds/WindowOpen.mp3",
   WINDOW_EXPAND: "/sounds/WindowExpand.mp3",
   WINDOW_COLLAPSE: "/sounds/WindowCollapse.mp3",
-  WINDOW_SWAP: "/sounds/WindowSwap.mp3",
   BUTTON_CLICK: "/sounds/ButtonClickDown.mp3",
   MENU_OPEN: "/sounds/MenuOpen.mp3",
   MENU_CLOSE: "/sounds/MenuClose.mp3",
