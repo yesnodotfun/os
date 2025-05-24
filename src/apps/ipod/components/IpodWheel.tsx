8069f47679baf934adf31ed04e728ed56ce58fe2
Ryo Lu <me@ryo.lu>
2025-05-23 23:22:52 -0700

    Update IpodWheel.tsx

--- src/apps/ipod/components/IpodWheel.tsx
+++ src/apps/ipod/components/IpodWheel.tsx
@@ -268,7 +268,7 @@ export function IpodWheel({
         <div className="absolute right-2 text-right top-1/2 transform -translate-y-1/2 font-chicago text-[16px] text-white cursor-default select-none">
           ⏭
         </div>
-        <div className="absolute bottom-1 text-center left-1/2 transform -translate-x-1/2 font-chicago text-[16px] text-white cursor-default select-none">
+        <div className="absolute bottom-0 text-center left-1/2 transform -translate-x-1/2 font-chicago text-[16px] text-white cursor-default select-none">
           ⏯
         </div>
         <div className="absolute left-2 text-left top-1/2 transform -translate-y-1/2 font-chicago text-[16px] text-white cursor-default select-none">
