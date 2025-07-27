f2371da35283ba0a32ebfc6eedf78184a6585e9f
Ryo Lu <me@ryo.lu>
2025-07-26 20:04:06 -0700

    style(finder): update text shadow for FileIcon component
    - Refined the text shadow styling in the FileIcon component for the macOS theme to enhance visual depth and clarity.
    - Improved the readability of the text shadow by using a more structured CSS syntax.

--- src/apps/finder/components/FileIcon.tsx
+++ src/apps/finder/components/FileIcon.tsx
@@ -280,7 +280,7 @@ export function FileIcon({
             ? isMacOSXTheme
               ? {
                   textShadow:
-                    "0 1px 0 rgba(0,0,0,0.9), 0 1px 2px rgba(0,0,0,0.85), 0 3px 6px rgba(0,0,0,0.65)",
+                    "rgba(0, 0, 0, 0.9) 0px 1px 0px, rgba(0, 0, 0, 0.85) 0px 1px 3px, rgba(0, 0, 0, 0.45) 0px 2px 3px",
                 }
               : { textShadow: "1px 1px 2px rgba(0, 0, 0, 0.8)" }
             : {}
